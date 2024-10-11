50347e764101eb96e3b6f9c955d58c5c6656a45d
Kleber Kruger <kleberkruger@gmail.com>
2024-10-11 08:30:21 -0400

    Spooler project

--- simulator.cpp
+++ simulator.cpp
@@ -14,11 +14,18 @@ std::thread Process::startPrintOrders(Spooler &spooler) const {
 }
 
 void Simulator::start(unsigned int num_process, unsigned int num_printers) {
-//    Printer printer1(1, 150);
-//    Printer printer2(2, 150);
-//    Printer printer3(3, 200);
+    Printer printer1(1);
+    Printer printer2(2);
+    Printer printer3(3);
 
-//    Spooler spooler(printer1, printer2, printer3);
+    Spooler sp0;
+    Spooler sp1(printer1);
+    Spooler sp2(printer1, printer2, printer3);
+    Spooler sp3(10);
+    Spooler sp4(10, printer1);
+    Spooler sp5(10, printer1, printer2, printer3);
+
+
 //    std::vector<std::pair<Process, std::thread>> process;
 //
 //    for (int pid = 1; pid <= num_process; pid++) {
--- spooler.hpp
+++ spooler.hpp
@@ -69,79 +69,17 @@ struct PrintJob {
     }
 };
 
-//class PrintJob {
-//public:
-//
-//    enum class Priority {
-//        VERY_HIGH = 1,
-//        HIGH = 2,
-//        MEDIUM = 3,
-//        LOW = 4,
-//        VERY_LOW = 5
-//    };
-//
-//    PrintJob(pid_t process, std::string_view file, unsigned long file_size,
-//             Priority priority = Priority::MEDIUM,
-//             std::chrono::system_clock::time_point request_time = std::chrono::system_clock::now()) :
-//             process(process), file(file), file_size(file_size), priority(priority), request_time(request_time) {}
-//
-//    PrintJob(const PrintJob &orig) = default;
-//
-//    [[nodiscard]] auto getProcess() const { return process; }
-//
-//    [[nodiscard]] const auto &getFile() const { return file; }
-//
-//    [[nodiscard]] auto getFileSize() const { return file_size; }
-//
-//    [[nodiscard]] auto getPriority() const { return priority; }
-//
-//    [[nodiscard]] const auto &getRequestTime() const { return request_time; }
-//
-//    [[nodiscard]] auto getRequestTimeStr() const { return timeToString(request_time); }
-//
-//    [[nodiscard]] const auto &getPrintTime() const { return print_time; }
-//
-//    [[nodiscard]] auto getPrintTimeStr() const { return timeToString(print_time); }
-//
-//    void setPrintTime(const auto &printTime) { print_time = printTime; }
-//
-//    static constexpr std::expected<Priority, std::string> priorityOf(int priority) {
-//        switch (priority) {
-//            case 1: return Priority::VERY_HIGH;
-//            case 2: return Priority::HIGH;
-//            case 3: return Priority::MEDIUM;
-//            case 4: return Priority::LOW;
-//            case 5: return Priority::VERY_LOW;
-//
-//            default: return std::unexpected("Invalid priority value");
-//        }
-//    }
-//
-//    static constexpr bool compare(const PrintJob &a, const PrintJob &b) {
-//        return (a.priority == b.priority) ? (a.request_time > b.request_time)
-//                                          : (a.priority > b.priority);
-//    }
-//
-//private:
-//
-//    pid_t process;
-//    std::string_view file;
-//    unsigned long file_size;
-//    Priority priority;
-//    std::chrono::system_clock::time_point request_time;
-//    std::chrono::system_clock::time_point print_time;
-//};
-
 using PrintPriority = PrintJob::Priority;
 
 class Printer {
 public:
-    explicit Printer(unsigned int id, std::pair<unsigned int, unsigned int> ppm = std::make_pair(30, 60)) :
+
+    explicit Printer(unsigned int id, std::pair<unsigned int, unsigned int> ppm = std::make_pair(60, 30)) :
             id(id), ppm() {}
 
     void printFile(PrintJob &job) {}
 
-    void printReport(PrintJob &request) const {
+    void printReport() const {
         logger::print("Relatório da impressora {}:\n", id);
         for (auto &p: prints) {
 //            logger::print("\tHora da requisição: {}, Hora da impressão: {}, Processo: {}, Arquivo: {}",
@@ -149,38 +87,63 @@ public:
         }
     }
 
+    [[nodiscard]] unsigned int getId() const { return id; }
+
+    [[nodiscard]] const std::vector<PrintJob> &getPrints() const { return prints; }
+
 private:
     unsigned int id;
     std::pair<unsigned int, unsigned int> ppm; // ppm: pages per minute<black-write, color>
     std::vector<PrintJob> prints;
 
-    auto calculatePrintLatency(std::string_view file, unsigned long file_size, unsigned int num_pages,
-                               unsigned int dpi) const;
+    [[nodiscard]] auto calculatePrintLatency(std::string_view file, unsigned long file_size, unsigned int num_pages,
+                                             unsigned int dpi) const;
 };
 
 class Spooler {
 public:
 
-//    explicit Spooler(unsigned long buffer_size = 10);
+    template<typename... Printers>
+    explicit Spooler(Printers &... printers) : buffer_capacity() {
+        addPrinters(printers...);
+        printPrinters();
+    }
 
     template<typename... Printers>
-    Spooler(unsigned long buffer_size = 10, Printers &... printers) : buffer_capacity(buffer_size) {
-
+    explicit Spooler(unsigned long buffer_capacity, Printers &... printers) : buffer_capacity(buffer_capacity) {
+        addPrinters(printers...);
+        printPrinters();
     }
 
     void addPrinter(Printer &printer) {}
 
     void removePrinter(unsigned int id) {}
 
+    void printPrinters() const {
+        for (const auto &printer: printers) {
+            std::cout << "Printer ID: " << printer.get().getId() << std::endl;
+        }
+    }
+
     void print(pid_t process, std::string_view file, unsigned long file_size,
                PrintPriority priority = PrintPriority::MEDIUM);
 
 private:
     std::priority_queue<PrintJob, std::vector<PrintJob>, decltype(&PrintJob::compare)> buffer;
+    std::vector<std::reference_wrapper<Printer>> printers;
     unsigned long buffer_capacity;
     std::mutex mutex;
     std::condition_variable cond_full;
     std::condition_variable cond_empty;
+    float simulation_velocity;
+
+    template<typename FirstPrinter, typename... RestPrinters>
+    void addPrinters(FirstPrinter &first, RestPrinters &... rest) {
+        printers.push_back(first);
+        addPrinters(rest...);
+    }
+
+    void addPrinters() {}
 
     void pushJob(const PrintJob &request);
 
